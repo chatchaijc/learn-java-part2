e98c57fe2a0633b8526dfbc41650767b72a35ece
chatchaijc <41831881+chatchaijc@users.noreply.github.com>
2021-04-16 23:03:13 +0700

    Create string example class

---
+++ LearnJavaPart2/src/com/askourtutors/learnJavaPart2/StringExample.java
@@ -0,0 +1,10 @@
+package com.askourtutors.learnJavaPart2;
+
+public class StringExample {
+	public static void main(String[] args) {
+		char[] myChar = { 'H', 'e' };
+		String myWord = new String(myChar);
+		System.out.println(myWord);
+
+	}
+}
